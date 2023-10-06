1650915d4cd87b60461eef4e18197bcf979dc74b
ZixuanXu0116 <zixuanxu@utexas.edu>
2023-10-06 12:39:16 -0500

    Add get_zipcode.py to get the zip code from given addresses

---
+++ get_zipcode.py
@@ -0,0 +1,30 @@
+import requests
+
+def get_zipcode(address, api_key):
+    # Define the base URL for the Google Maps Geocoding API
+    base_url = "https://maps.googleapis.com/maps/api/geocode/json"
+
+    # Parameters for the API request
+    params = {
+        "address": address,
+        "key": api_key,
+    }
+
+    try:
+        # Send a GET request to the API
+        response = requests.get(base_url, params=params)
+        data = response.json()
+
+        # Check if the response status is OK
+        if data["status"] == "OK":
+            # Extract the first result's postal code
+            result = data["results"][0]
+            for component in result["address_components"]:
+                if "postal_code" in component["types"]:
+                    return component["long_name"]
+
+        # If no zip code is found, return None
+        return None
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None

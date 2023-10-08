b36144027688e27542584b6cf8309000bbe7e81e
MUNAZZA BAWANY <munazzabawany@MUNAZZAs-Air.lan>
2023-10-08 18:26:36 -0500

    Fix issue #33: Living area vs rent

--- Code/analysis.py
+++ Code/analysis.py
@@ -54,7 +54,7 @@ plt.legend()
 plt.show()
 
 r2 = r2_score(y, y_predict)
-print(f'R-squared: {r2}')
+print(f'R-squared for distance: {r2}')
 
 #The coefficient is positive but small and the intercept is a big value. The relationship is positive but very little variance in rent is explained by distance from the unversity which means there are other factors playing a role. 
 
@@ -74,19 +74,19 @@ y_pred = model.predict(X1)
 
 r2_ = r2_score(y1, y_pred)
 
-print(f'R-squared: {r2_}')
+print(f'R-squared for distance and bedrooms: {r2_}')
 
 #Now taking both the variables into account gives a relatively bigger r2 which means a lot more variance in rent is explained by these two factors/variables. The relationship is negative with distance and positive with number of bedrooms as expected.
 
 #Lets see which zipcodes have the highest rents
 
-# Group data by zip code and calculate the average price for each zip code
+'''Group data by zip code and calculate the average price for each zip code'''
 zipcode_rents = df.groupby('Zip')['Price'].mean().reset_index()
 
-# Sort zip codes by average price in descending order
+'''Sort zip codes by average price in descending order'''
 zipcode_rents = zipcode_rents.sort_values(by='Price', ascending=False)
 
-# Plot the top N most expensive zip codes (i.e. top 10)
+'''Plot the top N most expensive zip codes (i.e. top 10)'''
 top_n = 10
 top_zipcodes = zipcode_rents['Zip'][:top_n].astype(str)
 top_rents = zipcode_rents['Price'][:top_n]
@@ -99,3 +99,38 @@ plt.title(f'Top {top_n} Most Expensive Zip Codes')
 plt.xticks(top_zipcodes, rotation=45)
 plt.show()
 
+#I think it would also be interesting to see the impact of living area on rents 
+
+X2 = df['LivingArea'].values.reshape(-1, 1)
+y2 = df['Price'].values
+
+model = LinearRegression()
+model.fit(X2,y2)
+
+print(f'Intercept: {model.intercept_}')
+print(f'Coefficient for living area: {model.coef_[0]}')
+
+y_predicted = model.predict(X2)
+
+plt.figure(figsize=(10, 6))
+plt.scatter(X2, y2, label='Original Data')
+plt.plot(X2, y_predicted, color='red', linewidth=2, label='Linear Regression')
+plt.xlabel('Living Area (sq. ft)')
+plt.ylabel('Rent')
+plt.title('Impact of Living Area on Rent')
+plt.legend()
+plt.grid(True)
+plt.show()
+
+r2__ = r2_score(y2, y_predicted)
+print(f'R-squared for living area: {r2__}')
+
+'''Calculate the correlation matrix'''
+correlation_matrix = df[['LivingArea', 'Price']].corr()
+
+'''Extract the correlation coefficient between LivingArea and Rent'''
+correlation_coefficient = correlation_matrix.loc['LivingArea', 'Price']
+
+print(f"Correlation Coefficient: {correlation_coefficient}")
+
+#There is a moderately strong positive relationship between living area and rent which means as living area increases then so does rent. Also, about 50% variability in rent is explained by living area

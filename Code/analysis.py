efe6d5744a21275eb4d915cd947e429aabc26ccf
MUNAZZA BAWANY <munazzabawany@MUNAZZAs-Air.lan>
2023-10-08 15:59:15 -0500

    Fix issue-27: Added analysis for distance

---
+++ Code/analysis.py
@@ -0,0 +1,83 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = pd.read_csv('artifacts/result.csv')
+
+print(df.head())
+print(df.info())
+print(df.describe())
+
+sns.scatterplot(x='Bedrooms', y='Price', data=df)
+plt.xlabel('Number of Rooms')
+plt.ylabel('Rent Price ($)')
+plt.title('Impact of Number of # of Rooms on Rent')
+plt.show()
+
+correlation = df['Bedrooms'].corr(df['Price'])
+print(f"Correlation between Number of Rooms and Rent Price: {correlation}")
+
+#We see a moderate positive linear relationship between two variables: Price and # of bedrooms. As # of bedrooms increase so does price moderately.
+
+#Now lets check the impact on price from the distance to university
+
+
+plt.scatter(df['Distance to the university'], df['Price'])
+plt.xlabel('Distance from University')
+plt.ylabel('Rental Rate')
+plt.title('Scatterplot of Distance vs. Rental Rate')
+plt.show()
+
+#We want to see how much variance in the rental rates is explained by distance from the university
+
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+
+X = df['Distance to the university'].str.extract('(\d+\.\d+)').astype(float)
+y = df['Price']
+
+model = LinearRegression()
+model.fit(X,y)
+
+
+print(f'Intercept: {model.intercept_}')
+print(f'Coefficient for Distance to the university: {model.coef_[0]}')
+
+y_predict = model.predict(X)
+
+plt.scatter(X, y, label='Data')
+plt.plot(X, y_predict, color='red', label='Regression Line')
+plt.xlabel('Distance from University')
+plt.ylabel('Rental Rate')
+plt.title('Linear Regression: Distance vs. Rental Rate')
+plt.legend()
+plt.show()
+
+r2 = r2_score(y, y_predict)
+print(f'R-squared: {r2}')
+
+#The coefficient is positive but small and the intercept is a big value. The relationship is positive but very little variance in rent is explained by distance from the unversity which means there are other factors playing a role. 
+
+#Lets see the effect taking both distance from university and number of bedrooms into account. 
+df['Distance to the university'] = df['Distance to the university'].str.extract('(\d+\.\d+)').astype(float)
+X1 = df[['Distance to the university', 'Bedrooms']]
+y1 = df['Price']
+
+# Create and fit a multiple linear regression model
+model = LinearRegression()
+model.fit(X1, y1)
+
+# Print the coefficients
+print(f'Intercept: {model.intercept_}')
+print(f'Coefficient for Distance to the university: {model.coef_[0]}')
+print(f'Coefficient for Bedrooms: {model.coef_[1]}')
+
+# Predict rental rates
+y_pred = model.predict(X1)
+
+# Evaluate the model
+r2_ = r2_score(y1, y_pred)
+
+print(f'R-squared: {r2_}')
+
+#Now taking both the variables into account gives a relatively bigger r2 which means a lot more variance in rent is explained by these two factors/variables. The relationship is negative with distance and positive with number of bedrooms as expected.

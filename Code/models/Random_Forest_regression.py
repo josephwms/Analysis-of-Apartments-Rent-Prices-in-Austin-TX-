b806f788280f64f276147ebf37f61cfadd13a9b0
Zixuan Xu <143112661+ZixuanXu0116@users.noreply.github.com>
2023-10-11 23:00:03 -0500

    Update Random_Forest_regression.py

--- Code/models/Random_Forest_regression.py
+++ Code/models/Random_Forest_regression.py
@@ -15,7 +15,10 @@ X = data.iloc[:, [1, 3, 5, 6, 7]]
 y = data.iloc[:, 0]
 
 # split data into training and testing set
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X,
+                                                    y,
+                                                    test_size=0.2,
+                                                    random_state=42)
 
 # Standardize X
 scaler = StandardScaler()
@@ -31,17 +34,15 @@ rf_regressor.fit(X_train_scaled, y_train)
 
 # Make predictions on the test data
 y_pred = rf_regressor.predict(X_test_scaled)
-
-
 ''''Evaluate the model, we don't have to print all the evaluation metrics, 
 just pick out those metrics that can clearly show the performance and easy to be interpreted.'''
 
 # evaluate the model
 # calculate the middle products
-n = X_test_scaled.shape[0] # number of observations
-p = X_test_scaled.shape[1] # number of coefficients except intercept
+n = X_test_scaled.shape[0]  # number of observations
+p = X_test_scaled.shape[1]  # number of coefficients except intercept
 residuals = y_test - rf_regressor.predict(X_test_scaled)
-rss = np.sum(residuals ** 2)
+rss = np.sum(residuals**2)
 
 # calculate the final results
 mse = mean_squared_error(y_test, y_pred)
@@ -50,24 +51,34 @@ explained_variance = explained_variance_score(y_test, y_pred)
 mae = mean_absolute_error(y_test, y_pred)
 adjusted_r_squared = 1 - (1 - r2) * ((n - 1) / (n - p - 1))
 aic = 2 * p + n * np.log(rss / n)
-bic = n * np.log(rss / n) + (p+1) * np.log(n)
+bic = n * np.log(rss / n) + (p + 1) * np.log(n)
 
 # show the result
-print(f"R-squared (R2) Score: {r2}")
-print(f"Adjusted R-squared: {adjusted_r_squared}")
-print(f"Mean Squared Error: {mse}")
-print(f"Mean Absolute Error: {mae}")
-print(f"AIC: {aic}")
-print(f"BIC: {bic}")
-
-
+print(f'R-squared (R2) Score: {r2}')
+print(f'Adjusted R-squared: {adjusted_r_squared}')
+print(f'Mean Squared Error: {mse}')
+print(f'Mean Absolute Error: {mae}')
+print(f'AIC: {aic}')
+print(f'BIC: {bic}')
 '''Interaction with users'''
 
-zip_code = str(input("Please input the expected zip code (in Austin, TX) of your apartment: ")) # input an integer like 78712, 78750, etc.
-distance = str(input("Please input the expected distance (in miles and keep two decimal places) to UT Austin of your apartment: ")) # input a float like 0.85, 1.56, 2.08, etc.
-bathrooms = str(input("Please input the expected number of bathrooms of your apartment: ")) # input an integer like 1, 2, 3, etc.
-bedrooms = str(input("Please input the expected number of bedrooms of your apartment: ")) # input an integer like 1, 2, 3, etc.
-living_area = str(input("Please input the expected living area in sqft. of your apartment: ")) # input a float like 1020, 805, 1874, etc.
+zip_code = str(
+    input(
+        'Please input the expected zip code (in Austin, TX) of your apartment: '
+    ))  # input an integer like 78712, 78750, etc.
+distance = str(
+    input(
+        'Please input the expected distance (in miles and keep two decimal places) to UT Austin of your apartment: '
+    ))  # input a float like 0.85, 1.56, 2.08, etc.
+bathrooms = str(
+    input('Please input the expected number of bathrooms of your apartment: ')
+)  # input an integer like 1, 2, 3, etc.
+bedrooms = str(
+    input('Please input the expected number of bedrooms of your apartment: ')
+)  # input an integer like 1, 2, 3, etc.
+living_area = str(
+    input('Please input the expected living area in sqft. of your apartment: ')
+)  # input a float like 1020, 805, 1874, etc.
 
 data_user = {
     'Zip': zip_code,
@@ -82,10 +93,3 @@ X_user_scaled = scaler.transform(X_user)
 Y_user = rf_regressor.predict(X_user_scaled)
 
 print(f'The estimated cost for your expected apartment is: {Y_user[0]}')
-
-
-
-
-
-
-

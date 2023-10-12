6fe686399072ec6502d40c996728db8511bacb8b
Zixuan Xu <143112661+ZixuanXu0116@users.noreply.github.com>
2023-10-11 22:56:42 -0500

    Update get_zipcode.py

--- Code/data/get_zipcode.py
+++ Code/data/get_zipcode.py
@@ -3,16 +3,17 @@ import dotenv
 import os
 
 dotenv.load_dotenv()
-APIKEY = os.environ["GOOGLE_APIKEY"]
+APIKEY = os.environ['GOOGLE_APIKEY']
+
 
 def get_zipcode(address):
     # Define the base URL for the Google Maps Geocoding API
-    base_url = "https://maps.googleapis.com/maps/api/geocode/json"
+    base_url = 'https://maps.googleapis.com/maps/api/geocode/json'
 
     # Parameters for the API request
     params = {
-        "address": address,
-        "key": APIKEY,
+        'address': address,
+        'key': APIKEY,
     }
 
     try:
@@ -21,15 +22,15 @@ def get_zipcode(address):
         data = response.json()
 
         # Check if the response status is OK
-        if data["status"] == "OK":
+        if data['status'] == 'OK':
             # Extract the first result's postal code
-            result = data["results"][0]
-            for component in result["address_components"]:
-                if "postal_code" in component["types"]:
-                    return component["long_name"]
+            result = data['results'][0]
+            for component in result['address_components']:
+                if 'postal_code' in component['types']:
+                    return component['long_name']
 
         # If no zip code is found, return None
         return None
     except Exception as e:
-        print(f"An error occurred: {e}")
+        print(f'An error occurred: {e}')
         return None

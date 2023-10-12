17a7673da007f83e26c63650c1135dcae225b3de
Zixuan Xu <143112661+ZixuanXu0116@users.noreply.github.com>
2023-10-11 22:59:46 -0500

    Update Polynomial_regression.py

--- Code/models/Polynomial_regression.py
+++ Code/models/Polynomial_regression.py
@@ -6,7 +6,6 @@ from sklearn.preprocessing import PolynomialFeatures
 from sklearn.preprocessing import StandardScaler
 import numpy as np
 
-
 # read the csv file
 data = pd.read_csv('artifacts/result.csv')
 
@@ -16,7 +15,10 @@ X = data.iloc[:, [1, 3, 5, 6, 7]]
 Y = data.iloc[:, 0]
 
 # split data into training and testing set
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+X_train, X_test, Y_train, Y_test = train_test_split(X,
+                                                    Y,
+                                                    test_size=0.2,
+                                                    random_state=42)
 
 # Standardize X
 scaler = StandardScaler()
@@ -34,10 +36,10 @@ Y_pred = model.predict(X_poly_test)
 
 # evaluate the model
 # calculate the middle products
-n = X_poly_test.shape[0] # numbers of observations
-p = X_poly_test.shape[1] # number of coefficients except intercept
+n = X_poly_test.shape[0]  # numbers of observations
+p = X_poly_test.shape[1]  # number of coefficients except intercept
 residuals = Y_test - model.predict(X_poly_test)
-rss = np.sum(residuals ** 2)
+rss = np.sum(residuals**2)
 
 # calculate the final results
 mse = mean_squared_error(Y_test, Y_pred)
@@ -46,14 +48,12 @@ explained_variance = explained_variance_score(Y_test, Y_pred)
 mae = mean_absolute_error(Y_test, Y_pred)
 adjusted_r_squared = 1 - (1 - r2) * ((n - 1) / (n - p - 1))
 aic = 2 * p + n * np.log(rss / n)
-bic = n * np.log(rss / n) + (p+1) * np.log(n)
+bic = n * np.log(rss / n) + (p + 1) * np.log(n)
 
 # show the result
-print(f"R-squared (R2) Score: {r2}")
-print(f"Adjusted R-squared: {adjusted_r_squared}")
-print(f"Mean Squared Error: {mse}")
-print(f"Mean Absolute Error: {mae}")
-print(f"AIC: {aic}")
-print(f"BIC: {bic}")
-
-
+print(f'R-squared (R2) Score: {r2}')
+print(f'Adjusted R-squared: {adjusted_r_squared}')
+print(f'Mean Squared Error: {mse}')
+print(f'Mean Absolute Error: {mae}')
+print(f'AIC: {aic}')
+print(f'BIC: {bic}')

8fa1182d88f1314bef75e0f9c7e24b3412f87f59
Zixuan Xu <143112661+ZixuanXu0116@users.noreply.github.com>
2023-10-11 22:56:22 -0500

    Update get_lat_lon.py

--- Code/data/get_lat_lon.py
+++ Code/data/get_lat_lon.py
@@ -3,30 +3,31 @@ import dotenv
 import os
 
 dotenv.load_dotenv()
-APIKEY = os.environ["GOOGLE_APIKEY"]
+APIKEY = os.environ['GOOGLE_APIKEY']
+
 
 def get_lat_long_by_university_name(university_name):
 
     # Construct the API request URL
-    base_url = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json"
+    base_url = 'https://maps.googleapis.com/maps/api/place/findplacefromtext/json'
     params = {
-        "input": university_name,
-        "inputtype": "textquery",
-        "fields": "geometry/location",
-        "key": APIKEY
+        'input': university_name,
+        'inputtype': 'textquery',
+        'fields': 'geometry/location',
+        'key': APIKEY
     }
 
     try:
         response = requests.get(base_url, params=params)
         data = response.json()
 
-        if response.status_code == 200 and data["status"] == "OK":
-            location = data["candidates"][0]["geometry"]["location"]
-            latitude = location["lat"]
-            longitude = location["lng"]
+        if response.status_code == 200 and data['status'] == 'OK':
+            location = data['candidates'][0]['geometry']['location']
+            latitude = location['lat']
+            longitude = location['lng']
             return latitude, longitude
         else:
-            print(f"Failed to retrieve coordinates. Status: {data['status']}")
+            print(f'Failed to retrieve coordinates. Status: {data["status"]}')
 
     except requests.exceptions.RequestException as e:
-        print(f"Error: {e}")
+        print(f'Error: {e}')
